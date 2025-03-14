8647e09afb04fb8d7e65e06fc157a797eb6fb9d8
Paix <paixellate@gmail.com>
2025-03-13 21:22:04 -0700

    Cleaning up project files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
 
 export default defineConfig({
+  plugins: [],
+  build: {
+    target: 'esnext'
+  },
   test: {
     globals: true,
+    environment: 'node'
   }
 })
